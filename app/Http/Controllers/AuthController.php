32a3b9689e7b4db1106f93354a46f1227683c920
MAUV90 <urban_a_90@hotmail.com>
2021-09-30 14:14:49 +0200

    La funcion de Login terminada

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,10 +4,31 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'email' =>['required', 'email'],
+            'password' => ['required'],
+        ]);
+    if (Auth::attempt($credentials)){
+        $usuarioLogeado = Auth::user();
+        $token = $usuarioLogeado->createToken('TokenUsuario')->plainTextToken;
+        $respuesta = [
+          'data' =>[
+              'usuario' => $usuarioLogeado,
+              'token' => $token
+          ],
+        ];
+        return response()->json($respuesta);
+    }else{
+        return response()->json(['error'=>'Unauthorised'], 401);
+    }
+    } 
     public function register(Request $request){
         $credentials = $request->validate([
             'name' => 'required',
